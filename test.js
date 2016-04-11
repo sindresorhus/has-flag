6260992dbe80ff4b32c21652d6d8225abe86c674
Mark Wubben <mark@novemberborn.net>
2016-04-11 16:37:56 +0100

    support short flags
    Allow flags to be prefixed with -- or -. The default prefix remains --, irrespective of flag length.

--- test.js
+++ test.js
@@ -6,6 +6,9 @@ test(t => {
 	t.true(m('--unicorn', ['--foo', '--unicorn', '--bar']), 'optional prefix');
 	t.true(m('unicorn=rainbow', ['--foo', '--unicorn=rainbow', '--bar']));
 	t.true(m('unicorn', ['--unicorn', '--', '--foo']));
+	t.true(m('-u', ['-f', '-u', '-b']));
+	t.true(m('-u', ['-u', '--', '-f']));
 	t.false(m('unicorn', ['--foo', '--', '--unicorn']), 'don\'t match flags after terminator');
 	t.false(m('unicorn', ['--foo']));
+	t.false(m('u', ['-f', '-u', '-b']), 'default prefix is --');
 });
