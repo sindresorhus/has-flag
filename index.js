ef614154ed24c9f659af89ee803692853deb8ded
Sindre Sorhus <sindresorhus@gmail.com>
2017-11-15 12:33:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 module.exports = function (flag, argv) {
 	argv = argv || process.argv;
 
-	const terminatorPos = argv.indexOf('--');
-	const prefix = /^-{1,2}/.test(flag) ? '' : '--';
-	let pos = argv.indexOf(prefix + flag);
+	var terminatorPos = argv.indexOf('--');
+	var prefix = /^-{1,2}/.test(flag) ? '' : '--';
+	var pos = argv.indexOf(prefix + flag);
 	pos = pos === -1 && prefix === '--' ? argv.indexOf('-' + flag) : pos;
 
 	return pos !== -1 && (terminatorPos === -1 ? true : pos < terminatorPos);
