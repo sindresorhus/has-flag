93a330877a00bb1147a9b7b16501d2f698fff0f5
Phred <fearphage@gmail.com>
2018-03-26 13:31:46 -0500

    Replaced ternary with or statement (#6)

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = (flag, argv) => {
 	const prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');
 	const pos = argv.indexOf(prefix + flag);
 	const terminatorPos = argv.indexOf('--');
-	return pos !== -1 && (terminatorPos === -1 ? true : pos < terminatorPos);
+	return pos !== -1 && (terminatorPos === -1 || pos < terminatorPos);
 };
