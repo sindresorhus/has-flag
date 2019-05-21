89abcccb86deb7f1b62f83a469fb3a14aea30262
Aron <aronadler@hotmail.com>
2019-05-21 16:00:00 +0100

    Make compatible with ES5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (flag, argv = process.argv) => {
+module.exports = function (flag, argv = process.argv) {
 	const prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');
 	const position = argv.indexOf(prefix + flag);
 	const terminatorPosition = argv.indexOf('--');
