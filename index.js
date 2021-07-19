0c2a6decdd40b64c7f30448eca921544312659bf
Sindre Sorhus <sindresorhus@gmail.com>
2021-07-19 17:30:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import process from 'process';
+import process from 'process'; // eslint-disable-line node/prefer-global/process
 
 export default function hasFlag(flag, argv = process.argv) {
 	const prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');
